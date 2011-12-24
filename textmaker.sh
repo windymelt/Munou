1a5774d8acea46c2624c3b664422a5906bd01a3d
Qwilas Windymelt <qwilas@mail.3qe.us>
2011-12-24 16:53:12 +0900

    原文テキストからの解析に成功。前処理スクリプト完成、解析プログラム第一段階達成

--- textmaker.sh
+++ textmaker.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-mecab -Owakati $1 | sed -e 's/\(.*\)/(\1)/g'> _marcov
+echo "(define *ORIGINAL* '(" >_markov
+mecab -Owakati $1 | sed -e 's/[ \t]$//g;/^$/d;s/^\(.*\)$/(\1)/g' >> _markov
+echo "))" >> _markov
